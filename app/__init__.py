401e5e3b76d051b6b9c1ddba27528138e2a63b3a
Daniel <dikey@email.cz>
2022-06-06 16:32:13 +0200

    changed database

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,6 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 ckeditor = CKEditor(app)
@@ -15,9 +13,7 @@ Bootstrap(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
-    basedir, "db/posts.db"
-)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db : SQLAlchemy = SQLAlchemy(app)
 
